32d0bad22c98708cf974246cdf2994e840bf3663
devpremoli <devpremoli@gmail.com>
2025-04-03 13:22:56 -0500

    if

--- control-flow/if-elseif-else.php
+++ control-flow/if-elseif-else.php
@@ -13,6 +13,16 @@ if ($is_user) {
     echo "<p>Welcome, User!</p>";
 }
 
+// else if
+if ($x > $y) {
+    $message = "<p>x is greater than y</p>";
+} else if ($x < $y) {
+    $message = "<p>x is less than y</p>";
+} else {
+    $message = "<p>x is equal to y</p>";
+}
+echo $message;
+
 // alternate syntax
 // else if gives error
 if ($is_user):
@@ -23,13 +33,13 @@ else:
     echo "<p>You are not a user</p>";
     echo "<p>Not a user</p>";
 endif;
+?>
 
-// else if
-if ($x > $y) {
-    $message = "<p>x is greater than y</p>";
-} else if ($x < $y) {
-    $message = "<p>x is less than y</p>";
-} else {
-    $message = "<p>x is equal to y</p>";
-}
-echo $message;
+<!-- better for HTML -->
+<?php if ($x > $y): ?>
+    <p>x is greater than y</p>
+<?php elseif ($x < $y): ?>
+    <p>x is less than y</p>
+<?php else: ?>
+    <p>x is equal to y</p>
+<?php endif ?>

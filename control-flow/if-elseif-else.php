0956357490742d0c844c64d7e95d944994684dab
devpremoli <devpremoli@gmail.com>
2025-04-03 13:12:49 -0500

    if elseif else

--- control-flow/if-elseif-else.php
+++ control-flow/if-elseif-else.php
@@ -1,37 +1,35 @@
 <?php
 
-$is_admin = true;
 $is_user = true;
-$can_edit = false;
-
-// good practice to use curly braces even if its a single statement
-if ($is_user)
-    echo "Welcom, User!\n";
-
-if ($is_admin) {
-    echo "Welcome, Admin!\n";
-    $can_edit = true;
-}
-
-
-$is_authenticated = false;
-
-if ($is_authenticated) {
-    echo "Authenticated!\n";
-} else {
-    echo "Not Authenticated\n";
-}
-
-
+$is_admin = true;
 $x = 10;
 $y = 20;
 
-if ($x > $y) {
-    $message = "x is greater than y\n";
-} elseif ($x < $y) {
-    $message = "x is less than y\n";
-} else {
-    $message = "x is equal to y\n";
+if ($is_user)
+    echo "<p>Welcome, User!</p>";
+
+// good practice to use curly braces even if its a single statement
+if ($is_user) {
+    echo "<p>Welcome, User!</p>";
 }
 
+// alternate syntax
+// else if gives error
+if ($is_user):
+    echo "<p>Welcome, User!</p>";
+elseif ($is_admin):
+    echo "<p>Welcome, User!</p>";
+else:
+    echo "<p>You are not a user</p>";
+    echo "<p>Not a user</p>";
+endif;
+
+// else if
+if ($x > $y) {
+    $message = "<p>x is greater than y</p>";
+} else if ($x < $y) {
+    $message = "<p>x is less than y</p>";
+} else {
+    $message = "<p>x is equal to y</p>";
+}
 echo $message;
